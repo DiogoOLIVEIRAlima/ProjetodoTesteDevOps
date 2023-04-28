1e85f81713faf64b878f6344df5ece0e6840681f
Diogo <limaa.diogi@gmail.com>
2023-04-28 16:29:47 -0300

    Fazendo Teste

---
+++ src/ClasseTeste.java
@@ -0,0 +1,7 @@
+public class ClasseTeste {
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario("Diogo", "123456789-10", "123456",
+                "14/04/2007", "14/04/2009", "manhã");
+        System.out.println(usuario);
+    }
+}
